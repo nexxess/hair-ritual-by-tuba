74dac5106fb9fae7cdd3c8b4c6fa1d29c1c7680a
Tobias Benkner <kontakt@tobias-benkner.de>
2026-02-17 20:29:04 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -9,6 +10,7 @@ const isProd = process.env.NODE_ENV === 'production';
 export default defineConfig({
   site: isProd ? 'https://nexxess.github.io' : 'http://localhost:4321',
   base: isProd ? '/hair-ritual-by-tuba' : '/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }

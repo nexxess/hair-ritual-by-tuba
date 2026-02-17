325271549fd091ae75f2f5d0fe1eade76de9b249
Tobias Benkner <kontakt@tobias-benkner.de>
2026-02-17 19:21:44 +0100

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
+  site: isProd ? 'https://nexxess.github.io' : 'http://localhost:4321',
+  base: isProd ? '/hair-ritual-by-tuba' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
